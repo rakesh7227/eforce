satchi2k2/efcore

Metadata work for json. Separated from main PR to make the review easier. This PR includes:
- builder methods,
- conventions,
- relational model,
- model validation,
- migrations,
- update

src/EFCore.Relational/Extensions/RelationalNavigationBuilderExtensions.cs
@@ -1,0 +1,64 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.EntityFrameworkCore;
+
+/// <summary>
+///     Relational database specific extension methods for <see cref="NavigationBuilder" />.
+/// </summary>
+/// <remarks>
+///     See <see href="https://aka.ms/efcore-docs-modeling">Modeling entity types and relationships</see> for more information and examples.
+/// </remarks>
+public static class RelationalNavigationBuilderExtensions
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static NavigationBuilder HasJsonElementName(
+        this NavigationBuilder navigationBuilder,
+        string? name)
+    {
+        Check.NullButNotEmpty(name, nameof(name));
+
+        navigationBuilder.Metadata.SetJsonElementName(name);
+
+        return navigationBuilder;
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static NavigationBuilder<TSource, TTarget> HasJsonElementName<TSource, TTarget>(
+        this NavigationBuilder<TSource, TTarget> navigationBuilder,
+        string? name)
+        where TSource : class
+        where TTarget : class
+        => (NavigationBuilder<TSource, TTarget>)HasJsonElementName((NavigationBuilder)navigationBuilder, name);
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static IConventionNavigationBuilder? HasJsonElementName(
+        this IConventionNavigationBuilder navigationBuilder,
+        string? name,
+        bool fromDataAnnotation = false)
+    {
+        if (!navigationBuilder.CanSetJsonElementName(name, fromDataAnnotation))
+        {
+            return null;
+        }
+
+        navigationBuilder.Metadata.SetJsonElementName(name, fromDataAnnotation);
+
+        return navigationBuilder;
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static bool CanSetJsonElementName(
+        this IConventionNavigationBuilder navigationBuilder,
+        string? name,
+        bool fromDataAnnotation = false)
+        => navigationBuilder.CanSetAnnotation(RelationalAnnotationNames.JsonElementName, name, fromDataAnnotation);
+}

src/EFCore.Relational/Extensions/RelationalNavigationExtensions.cs
@@ -1,0 +1,49 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.EntityFrameworkCore;
+
+/// <summary>
+///     Navigation extension methods for relational database metadata.
+/// </summary>
+/// <remarks>
+///     See <see href="https://aka.ms/efcore-docs-modeling">Modeling entity types and relationships</see> for more information and examples.
+/// </remarks>
+public static class RelationalNavigationExtensions
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static string JsonElementName(this IReadOnlyNavigationBase navigation)
+        => (string?)navigation.FindAnnotation(RelationalAnnotationNames.JsonElementName)?.Value ?? navigation.Name;
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static void SetJsonElementName(this IMutableNavigationBase navigation, string? name)
+        => navigation.SetOrRemoveAnnotation(
+            RelationalAnnotationNames.JsonElementName,
+            Check.NullButNotEmpty(name, nameof(name)));
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static string? SetJsonElementName(
+        this IConventionNavigationBase navigation,
+        string? name,
+        bool fromDataAnnotation = false)
+    {
+        navigation.SetOrRemoveAnnotation(
+            RelationalAnnotationNames.JsonElementName,
+            Check.NullButNotEmpty(name, nameof(name)),
+            fromDataAnnotation);
+
+        return name;
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static ConfigurationSource? GetJsonElementNameConfigurationSource(this IConventionNavigationBase navigation)
+        => navigation.FindAnnotation(RelationalAnnotationNames.JsonElementName)?.GetConfigurationSource();
+}

src/EFCore.Relational/Metadata/Conventions/RelationalJsonPropertyNameAttributeConvention.cs
@@ -1,0 +1,52 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text.Json.Serialization;
+
+namespace Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+/// <summary>
+///     A convention that configures json property name and type for a property based on the applied <see cref="JsonPropertyNameAttribute" />.
+/// </summary>
+/// <remarks>
+///     See <see href="https://aka.ms/efcore-docs-conventions">Model building conventions</see> for more information and examples.
+/// </remarks>
+public class RelationalJsonPropertyNameAttributeConvention : PropertyAttributeConventionBase<JsonPropertyNameAttribute>
+{
+    /// <summary>
+    ///     Creates a new instance of <see cref="RelationalJsonPropertyNameAttributeConvention" />.
+    /// </summary>
+    /// <param name="dependencies">Parameter object containing dependencies for this convention.</param>
+    /// <param name="relationalDependencies"> Parameter object containing relational dependencies for this convention.</param>
+    public RelationalJsonPropertyNameAttributeConvention(
+        ProviderConventionSetBuilderDependencies dependencies,
+        RelationalConventionSetBuilderDependencies relationalDependencies)
+        : base(dependencies)
+    {
+        RelationalDependencies = relationalDependencies;
+    }
+
+    /// <summary>
+    ///     Relational provider-specific dependencies for this service.
+    /// </summary>
+    protected virtual RelationalConventionSetBuilderDependencies RelationalDependencies { get; }
+
+    /// <summary>
+    ///     Called after a property is added to the entity type with an attribute on the associated CLR property or field.
+    /// </summary>
+    /// <param name="propertyBuilder">The builder for the property.</param>
+    /// <param name="attribute">The attribute.</param>
+    /// <param name="clrMember">The member that has the attribute.</param>
+    /// <param name="context">Additional information associated with convention execution.</param>
+    protected override void ProcessPropertyAdded(
+        IConventionPropertyBuilder propertyBuilder,
+        JsonPropertyNameAttribute attribute,
+        MemberInfo clrMember,
+        IConventionContext context)
+    {
+        if (!string.IsNullOrWhiteSpace(attribute.Name))
+        {
+            propertyBuilder.HasJsonElementName(attribute.Name, fromDataAnnotation: true);
+        }
+    }
+}

src/EFCore.Relational/Metadata/Conventions/RelationalNavigationJsonPropertyNameAttributeConvention.cs
@@ -1,0 +1,51 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text.Json.Serialization;
+
+namespace Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+/// <summary>
+///     A convention that configures a json element name for the navigation property mapped to json
+///     based on the <see cref="JsonPropertyNameAttribute" /> attribute.
+/// </summary>
+/// <remarks>
+///     See <see href="https://aka.ms/efcore-docs-conventions">Model building conventions</see> for more information and examples.
+/// </remarks>
+public class RelationalNavigationJsonPropertyNameAttributeConvention : NavigationAttributeConventionBase<JsonPropertyNameAttribute>
+{
+    /// <summary>
+    ///     Creates a new instance of <see cref="RelationalNavigationJsonPropertyNameAttributeConvention" />.
+    /// </summary>
+    /// <param name="dependencies">Parameter object containing dependencies for this convention.</param>
+    /// <param name="relationalDependencies"> Parameter object containing relational dependencies for this convention.</param>
+    public RelationalNavigationJsonPropertyNameAttributeConvention(
+        ProviderConventionSetBuilderDependencies dependencies,
+        RelationalConventionSetBuilderDependencies relationalDependencies)
+        : base(dependencies)
+    {
+        RelationalDependencies = relationalDependencies;
+    }
+
+    /// <summary>
+    ///     Relational provider-specific dependencies for this service.
+    /// </summary>
+    protected virtual RelationalConventionSetBuilderDependencies RelationalDependencies { get; }
+
+    /// <summary>
+    ///     Called after a navigation property that has an attribute is added to an entity type.
+    /// </summary>
+    /// <param name="navigationBuilder">The builder for the navigation.</param>
+    /// <param name="attribute">The attribute.</param>
+    /// <param name="context">Additional information associated with convention execution.</param>
+    public override void ProcessNavigationAdded(
+        IConventionNavigationBuilder navigationBuilder,
+        JsonPropertyNameAttribute attribute,
+        IConventionContext<IConventionNavigationBuilder> context)
+    {
+        if (!string.IsNullOrWhiteSpace(attribute.Name))
+        {
+            navigationBuilder.HasJsonElementName(attribute.Name, fromDataAnnotation: true);
+        }
+    }
+}

src/EFCore.Relational/Metadata/Internal/JsonColumn.cs
@@ -1,0 +1,88 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.EntityFrameworkCore.Metadata.Internal
+{
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public class JsonColumn : Column, IColumn
+    {
+        private readonly ValueComparer _provierValueComparer;
+
+        /// <summary>
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+        /// </summary>
+        public JsonColumn(string name, string type, Table table, ValueComparer provierValueComparer)
+            : base(name, type, table)
+        {
+            _provierValueComparer = provierValueComparer;
+        }
+
+        /// <inheritdoc />
+        int? IColumn.MaxLength
+            => null;
+
+        /// <inheritdoc />
+        int? IColumn.Precision
+            => null;
+
+        /// <inheritdoc />
+        int? IColumn.Scale
+            => null;
+
+        /// <inheritdoc />
+        bool? IColumn.IsUnicode
+            => null;
+
+        /// <inheritdoc />
+        bool? IColumn.IsFixedLength
+            => null;
+
+        /// <inheritdoc />
+        bool IColumn.IsRowVersion
+            => false;
+
+        /// <inheritdoc />
+        int? IColumn.Order
+            => null;
+
+        /// <inheritdoc />
+        object? IColumn.DefaultValue
+            => null;
+
+        /// <inheritdoc />
+        string? IColumn.DefaultValueSql
+            => null;
+
+        /// <inheritdoc />
+        string? IColumn.ComputedColumnSql
+            => null;
+
+        /// <inheritdoc />
+        bool? IColumn.IsStored
+            => null;
+
+        /// <inheritdoc />
+        string? IColumn.Comment
+            => null;
+
+        /// <inheritdoc />
+        string? IColumn.Collation
+            => null;
+
+        /// <inheritdoc />
+        ValueComparer IColumn.ProviderValueComparer
+            => _provierValueComparer;
+
+        /// <inheritdoc />
+        IColumnMapping? IColumn.FindColumnMapping(IReadOnlyEntityType entityType)
+            => null;
+    }
+}

rakesh7227/eforce

src/EFCore.Relational/Extensions/RelationalNavigationBuilderExtensions.cs
@@ -1,0 +1,64 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.EntityFrameworkCore;
+
+/// <summary>
+///     Relational database specific extension methods for <see cref="NavigationBuilder" />.
+/// </summary>
+/// <remarks>
+///     See <see href="https://aka.ms/efcore-docs-modeling">Modeling entity types and relationships</see> for more information and examples.
+/// </remarks>
+public static class RelationalNavigationBuilderExtensions
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static NavigationBuilder HasJsonPropertyName(
+        this NavigationBuilder navigationBuilder,
+        string? name)
+    {
+        Check.NullButNotEmpty(name, nameof(name));
+
+        navigationBuilder.Metadata.SetJsonPropertyName(name);
+
+        return navigationBuilder;
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static NavigationBuilder<TSource, TTarget> HasJsonPropertyName<TSource, TTarget>(
+        this NavigationBuilder<TSource, TTarget> navigationBuilder,
+        string? name)
+        where TSource : class
+        where TTarget : class
+        => (NavigationBuilder<TSource, TTarget>)HasJsonPropertyName((NavigationBuilder)navigationBuilder, name);
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static IConventionNavigationBuilder? HasJsonPropertyName(
+        this IConventionNavigationBuilder navigationBuilder,
+        string? name,
+        bool fromDataAnnotation = false)
+    {
+        if (!navigationBuilder.CanSetJsonPropertyName(name, fromDataAnnotation))
+        {
+            return null;
+        }
+
+        navigationBuilder.Metadata.SetJsonPropertyName(name, fromDataAnnotation);
+
+        return navigationBuilder;
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static bool CanSetJsonPropertyName(
+        this IConventionNavigationBuilder navigationBuilder,
+        string? name,
+        bool fromDataAnnotation = false)
+        => navigationBuilder.CanSetAnnotation(RelationalAnnotationNames.JsonPropertyName, name, fromDataAnnotation);
+}

src/EFCore.Relational/Extensions/RelationalNavigationExtensions.cs
@@ -1,0 +1,49 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.EntityFrameworkCore;
+
+/// <summary>
+///     Navigation extension methods for relational database metadata.
+/// </summary>
+/// <remarks>
+///     See <see href="https://aka.ms/efcore-docs-modeling">Modeling entity types and relationships</see> for more information and examples.
+/// </remarks>
+public static class RelationalNavigationExtensions
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static string GetJsonPropertyName(this IReadOnlyNavigationBase navigation)
+        => (string?)navigation.FindAnnotation(RelationalAnnotationNames.JsonPropertyName)?.Value ?? navigation.Name;
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static void SetJsonPropertyName(this IMutableNavigationBase navigation, string? name)
+        => navigation.SetOrRemoveAnnotation(
+            RelationalAnnotationNames.JsonPropertyName,
+            Check.NullButNotEmpty(name, nameof(name)));
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static string? SetJsonPropertyName(
+        this IConventionNavigationBase navigation,
+        string? name,
+        bool fromDataAnnotation = false)
+    {
+        navigation.SetOrRemoveAnnotation(
+            RelationalAnnotationNames.JsonPropertyName,
+            Check.NullButNotEmpty(name, nameof(name)),
+            fromDataAnnotation);
+
+        return name;
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static ConfigurationSource? GetJsonPropertyNameConfigurationSource(this IConventionNavigationBase navigation)
+        => navigation.FindAnnotation(RelationalAnnotationNames.JsonPropertyName)?.GetConfigurationSource();
+}

src/EFCore.Relational/Metadata/Conventions/RelationalJsonPropertyNameAttributeConvention.cs
@@ -1,0 +1,52 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text.Json.Serialization;
+
+namespace Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+/// <summary>
+///     A convention that configures json property name and type for a property based on the applied <see cref="JsonPropertyNameAttribute" />.
+/// </summary>
+/// <remarks>
+///     See <see href="https://aka.ms/efcore-docs-conventions">Model building conventions</see> for more information and examples.
+/// </remarks>
+public class RelationalJsonPropertyNameAttributeConvention : PropertyAttributeConventionBase<JsonPropertyNameAttribute>
+{
+    /// <summary>
+    ///     Creates a new instance of <see cref="RelationalJsonPropertyNameAttributeConvention" />.
+    /// </summary>
+    /// <param name="dependencies">Parameter object containing dependencies for this convention.</param>
+    /// <param name="relationalDependencies"> Parameter object containing relational dependencies for this convention.</param>
+    public RelationalJsonPropertyNameAttributeConvention(
+        ProviderConventionSetBuilderDependencies dependencies,
+        RelationalConventionSetBuilderDependencies relationalDependencies)
+        : base(dependencies)
+    {
+        RelationalDependencies = relationalDependencies;
+    }
+
+    /// <summary>
+    ///     Relational provider-specific dependencies for this service.
+    /// </summary>
+    protected virtual RelationalConventionSetBuilderDependencies RelationalDependencies { get; }
+
+    /// <summary>
+    ///     Called after a property is added to the entity type with an attribute on the associated CLR property or field.
+    /// </summary>
+    /// <param name="propertyBuilder">The builder for the property.</param>
+    /// <param name="attribute">The attribute.</param>
+    /// <param name="clrMember">The member that has the attribute.</param>
+    /// <param name="context">Additional information associated with convention execution.</param>
+    protected override void ProcessPropertyAdded(
+        IConventionPropertyBuilder propertyBuilder,
+        JsonPropertyNameAttribute attribute,
+        MemberInfo clrMember,
+        IConventionContext context)
+    {
+        if (!string.IsNullOrWhiteSpace(attribute.Name))
+        {
+            propertyBuilder.HasJsonPropertyName(attribute.Name, fromDataAnnotation: true);
+        }
+    }
+}

src/EFCore.Relational/Metadata/Conventions/RelationalNavigationJsonPropertyNameAttributeConvention.cs
@@ -1,0 +1,51 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text.Json.Serialization;
+
+namespace Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+/// <summary>
+///     A convention that configures a json element name for the navigation property mapped to json
+///     based on the <see cref="JsonPropertyNameAttribute" /> attribute.
+/// </summary>
+/// <remarks>
+///     See <see href="https://aka.ms/efcore-docs-conventions">Model building conventions</see> for more information and examples.
+/// </remarks>
+public class RelationalNavigationJsonPropertyNameAttributeConvention : NavigationAttributeConventionBase<JsonPropertyNameAttribute>
+{
+    /// <summary>
+    ///     Creates a new instance of <see cref="RelationalNavigationJsonPropertyNameAttributeConvention" />.
+    /// </summary>
+    /// <param name="dependencies">Parameter object containing dependencies for this convention.</param>
+    /// <param name="relationalDependencies"> Parameter object containing relational dependencies for this convention.</param>
+    public RelationalNavigationJsonPropertyNameAttributeConvention(
+        ProviderConventionSetBuilderDependencies dependencies,
+        RelationalConventionSetBuilderDependencies relationalDependencies)
+        : base(dependencies)
+    {
+        RelationalDependencies = relationalDependencies;
+    }
+
+    /// <summary>
+    ///     Relational provider-specific dependencies for this service.
+    /// </summary>
+    protected virtual RelationalConventionSetBuilderDependencies RelationalDependencies { get; }
+
+    /// <summary>
+    ///     Called after a navigation property that has an attribute is added to an entity type.
+    /// </summary>
+    /// <param name="navigationBuilder">The builder for the navigation.</param>
+    /// <param name="attribute">The attribute.</param>
+    /// <param name="context">Additional information associated with convention execution.</param>
+    public override void ProcessNavigationAdded(
+        IConventionNavigationBuilder navigationBuilder,
+        JsonPropertyNameAttribute attribute,
+        IConventionContext<IConventionNavigationBuilder> context)
+    {
+        if (!string.IsNullOrWhiteSpace(attribute.Name))
+        {
+            navigationBuilder.HasJsonPropertyName(attribute.Name, fromDataAnnotation: true);
+        }
+    }
+}

src/EFCore.Relational/Metadata/Internal/JsonColumn.cs
@@ -1,0 +1,87 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+/// <summary>
+///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+///     any release. You should only use it directly in your code with extreme caution and knowing that
+///     doing so can result in application failures when updating to a new Entity Framework Core release.
+/// </summary>
+public class JsonColumn : Column, IColumn
+{
+    private readonly ValueComparer _providerValueComparer;
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public JsonColumn(string name, string type, Table table, ValueComparer provierValueComparer)
+        : base(name, type, table)
+    {
+        _providerValueComparer = provierValueComparer;
+    }
+
+    /// <inheritdoc />
+    int? IColumn.MaxLength
+        => null;
+
+    /// <inheritdoc />
+    int? IColumn.Precision
+        => null;
+
+    /// <inheritdoc />
+    int? IColumn.Scale
+        => null;
+
+    /// <inheritdoc />
+    bool? IColumn.IsUnicode
+        => null;
+
+    /// <inheritdoc />
+    bool? IColumn.IsFixedLength
+        => null;
+
+    /// <inheritdoc />
+    bool IColumn.IsRowVersion
+        => false;
+
+    /// <inheritdoc />
+    int? IColumn.Order
+        => null;
+
+    /// <inheritdoc />
+    object? IColumn.DefaultValue
+        => null;
+
+    /// <inheritdoc />
+    string? IColumn.DefaultValueSql
+        => null;
+
+    /// <inheritdoc />
+    string? IColumn.ComputedColumnSql
+        => null;
+
+    /// <inheritdoc />
+    bool? IColumn.IsStored
+        => null;
+
+    /// <inheritdoc />
+    string? IColumn.Comment
+        => null;
+
+    /// <inheritdoc />
+    string? IColumn.Collation
+        => null;
+
+    /// <inheritdoc />
+    ValueComparer IColumn.ProviderValueComparer
+        => _providerValueComparer;
+
+    /// <inheritdoc />
+    IColumnMapping? IColumn.FindColumnMapping(IReadOnlyEntityType entityType)
+        => null;
+}