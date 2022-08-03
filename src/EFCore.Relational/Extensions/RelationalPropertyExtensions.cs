satchi2k2/efcore

Metadata work for json. Separated from main PR to make the review easier. This PR includes:
- builder methods,
- conventions,
- relational model,
- model validation,
- migrations,
- update

src/EFCore.Relational/Extensions/RelationalPropertyExtensions.cs
@@ -169,8 +169,13 @@ public static class RelationalPropertyExtensions
     /// <param name="property">The property.</param>
     /// <param name="storeObject">The identifier of the table-like store object containing the column.</param>
     /// <returns>The default column name to which the property would be mapped.</returns>
-    public static string GetDefaultColumnName(this IReadOnlyProperty property, in StoreObjectIdentifier storeObject)
+    public static string? GetDefaultColumnName(this IReadOnlyProperty property, in StoreObjectIdentifier storeObject)
     {
+        if (property.DeclaringEntityType.IsMappedToJson())
+        {
+            return null;
+        }
+
         var sharedTablePrincipalPrimaryKeyProperty = FindSharedObjectRootPrimaryKeyProperty(property, storeObject);
         if (sharedTablePrincipalPrimaryKeyProperty != null)
         {
@@ -1446,6 +1451,12 @@ public static class RelationalPropertyExtensions
 
     private static IReadOnlyProperty? FindSharedObjectRootProperty(IReadOnlyProperty property, in StoreObjectIdentifier storeObject)
     {
+        if (property.DeclaringEntityType.IsMappedToJson())
+        {
+            //JSON-splitting is not supported
+            return null;
+        }
+
         var column = property.GetColumnName(storeObject);
         if (column == null)
         {
@@ -1500,6 +1511,17 @@ public static class RelationalPropertyExtensions
         {
             var linkingRelationship = principalProperty.DeclaringEntityType
                 .FindRowInternalForeignKeys(storeObject).FirstOrDefault();
+
+            // principal is mapped to json, but the property we are fetching the shared object root for is not
+            // this can happen only when the model is not fully configured yet, parent is already marked as json,
+            // the child is not marked yet, but it will be (mix of owned entities and non-owned is not supported)
+            // in this case, we should still find the linking relationship, even though we normally don't do it for the json entities
+            // just so we can complete configuring the relationships
+            if (linkingRelationship == null && principalProperty.DeclaringEntityType.IsMappedToJson())
+            {
+                linkingRelationship = FindRowInternalForeignKeyForJson(principalProperty.DeclaringEntityType, storeObject);
+            }
+
             if (linkingRelationship == null)
             {
                 break;
@@ -1509,6 +1531,38 @@ public static class RelationalPropertyExtensions
         }
 
         return principalProperty == property ? null : principalProperty;
+    }
+
+    private static IReadOnlyForeignKey? FindRowInternalForeignKeyForJson(
+        IReadOnlyEntityType entityType,
+        StoreObjectIdentifier storeObject)
+    {
+        var primaryKey = entityType.FindPrimaryKey();
+        if (primaryKey == null)
+        {
+            return null;
+        }
+
+        foreach (var foreignKey in entityType.GetForeignKeys())
+        {
+            if (!foreignKey.PrincipalKey.IsPrimaryKey()
+                || foreignKey.PrincipalEntityType.IsAssignableFrom(foreignKey.DeclaringEntityType)
+                || !foreignKey.Properties.SequenceEqual(primaryKey.Properties)
+                || !IsMapped(foreignKey, storeObject))
+            {
+                continue;
+            }
+
+            return foreignKey;
+        }
+
+        return null;
+
+        static bool IsMapped(IReadOnlyForeignKey foreignKey, StoreObjectIdentifier storeObject)
+            => (StoreObjectIdentifier.Create(foreignKey.DeclaringEntityType, storeObject.StoreObjectType) == storeObject
+                    || foreignKey.DeclaringEntityType.GetMappingFragments(storeObject.StoreObjectType).Any(f => f.StoreObject == storeObject))
+                && (StoreObjectIdentifier.Create(foreignKey.PrincipalEntityType, storeObject.StoreObjectType) == storeObject
+                    || foreignKey.PrincipalEntityType.GetMappingFragments(storeObject.StoreObjectType).Any(f => f.StoreObject == storeObject));
     }
 
     private static IReadOnlyProperty? FindSharedObjectRootConcurrencyTokenProperty(
@@ -1958,4 +2012,40 @@ public static class RelationalPropertyExtensions
 
         throw new InvalidOperationException(message, exception);
     }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static string JsonElementName(this IReadOnlyProperty property)
+        => (string?)property.FindAnnotation(RelationalAnnotationNames.JsonElementName)?.Value ?? property.Name;
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static void SetJsonElementName(this IMutableProperty property, string? name)
+        => property.SetOrRemoveAnnotation(
+            RelationalAnnotationNames.JsonElementName,
+            Check.NullButNotEmpty(name, nameof(name)));
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static string? SetJsonElementName(
+        this IConventionProperty property,
+        string? name,
+        bool fromDataAnnotation = false)
+    {
+        property.SetOrRemoveAnnotation(
+            RelationalAnnotationNames.JsonElementName,
+            Check.NullButNotEmpty(name, nameof(name)),
+            fromDataAnnotation);
+
+        return name;
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static ConfigurationSource? GetJsonElementNameConfigurationSource(this IConventionProperty property)
+        => property.FindAnnotation(RelationalAnnotationNames.JsonElementName)?.GetConfigurationSource();
 }

src/EFCore.Relational/Metadata/Conventions/RelationalNavigationJsonPropertyNameAttributeConvention.cs
@@ -1,0 +1,51 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text.Json.Serialization;
+
+namespace Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+/// <summary>
+///     A convention that configures a json element name for the navigation property mapped to json
+///     based on the <see cref="JsonPropertyNameAttribute" /> attribute.
+/// </summary>
+/// <remarks>
+///     See <see href="https://aka.ms/efcore-docs-conventions">Model building conventions</see> for more information and examples.
+/// </remarks>
+public class RelationalNavigationJsonPropertyNameAttributeConvention : NavigationAttributeConventionBase<JsonPropertyNameAttribute>
+{
+    /// <summary>
+    ///     Creates a new instance of <see cref="RelationalNavigationJsonPropertyNameAttributeConvention" />.
+    /// </summary>
+    /// <param name="dependencies">Parameter object containing dependencies for this convention.</param>
+    /// <param name="relationalDependencies"> Parameter object containing relational dependencies for this convention.</param>
+    public RelationalNavigationJsonPropertyNameAttributeConvention(
+        ProviderConventionSetBuilderDependencies dependencies,
+        RelationalConventionSetBuilderDependencies relationalDependencies)
+        : base(dependencies)
+    {
+        RelationalDependencies = relationalDependencies;
+    }
+
+    /// <summary>
+    ///     Relational provider-specific dependencies for this service.
+    /// </summary>
+    protected virtual RelationalConventionSetBuilderDependencies RelationalDependencies { get; }
+
+    /// <summary>
+    ///     Called after a navigation property that has an attribute is added to an entity type.
+    /// </summary>
+    /// <param name="navigationBuilder">The builder for the navigation.</param>
+    /// <param name="attribute">The attribute.</param>
+    /// <param name="context">Additional information associated with convention execution.</param>
+    public override void ProcessNavigationAdded(
+        IConventionNavigationBuilder navigationBuilder,
+        JsonPropertyNameAttribute attribute,
+        IConventionContext<IConventionNavigationBuilder> context)
+    {
+        if (!string.IsNullOrWhiteSpace(attribute.Name))
+        {
+            navigationBuilder.HasJsonElementName(attribute.Name, fromDataAnnotation: true);
+        }
+    }
+}

rakesh7227/eforce

src/EFCore.Relational/Extensions/RelationalPropertyExtensions.cs
@@ -4,7 +4,6 @@
 using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Text;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 // ReSharper disable once CheckNamespace
@@ -169,8 +168,13 @@ public static class RelationalPropertyExtensions
     /// <param name="property">The property.</param>
     /// <param name="storeObject">The identifier of the table-like store object containing the column.</param>
     /// <returns>The default column name to which the property would be mapped.</returns>
-    public static string GetDefaultColumnName(this IReadOnlyProperty property, in StoreObjectIdentifier storeObject)
+    public static string? GetDefaultColumnName(this IReadOnlyProperty property, in StoreObjectIdentifier storeObject)
     {
+        if (property.DeclaringEntityType.IsMappedToJson())
+        {
+            return null;
+        }
+
         var sharedTablePrincipalPrimaryKeyProperty = FindSharedObjectRootPrimaryKeyProperty(property, storeObject);
         if (sharedTablePrincipalPrimaryKeyProperty != null)
         {
@@ -1446,6 +1450,12 @@ public static class RelationalPropertyExtensions
 
     private static IReadOnlyProperty? FindSharedObjectRootProperty(IReadOnlyProperty property, in StoreObjectIdentifier storeObject)
     {
+        if (property.DeclaringEntityType.IsMappedToJson())
+        {
+            //JSON-splitting is not supported
+            return null;
+        }
+
         var column = property.GetColumnName(storeObject);
         if (column == null)
         {
@@ -1500,6 +1510,7 @@ public static class RelationalPropertyExtensions
         {
             var linkingRelationship = principalProperty.DeclaringEntityType
                 .FindRowInternalForeignKeys(storeObject).FirstOrDefault();
+
             if (linkingRelationship == null)
             {
                 break;
@@ -1958,4 +1969,41 @@ public static class RelationalPropertyExtensions
 
         throw new InvalidOperationException(message, exception);
     }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static string? GetJsonPropertyName(this IReadOnlyProperty property)
+        => (string?)property.FindAnnotation(RelationalAnnotationNames.JsonPropertyName)?.Value
+            ?? (property.IsKey() || !property.DeclaringEntityType.IsMappedToJson() ? null : property.Name);
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static void SetJsonPropertyName(this IMutableProperty property, string? name)
+        => property.SetOrRemoveAnnotation(
+            RelationalAnnotationNames.JsonPropertyName,
+            Check.NullButNotEmpty(name, nameof(name)));
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static string? SetJsonPropertyName(
+        this IConventionProperty property,
+        string? name,
+        bool fromDataAnnotation = false)
+    {
+        property.SetOrRemoveAnnotation(
+            RelationalAnnotationNames.JsonPropertyName,
+            Check.NullButNotEmpty(name, nameof(name)),
+            fromDataAnnotation);
+
+        return name;
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static ConfigurationSource? GetJsonPropertyNameConfigurationSource(this IConventionProperty property)
+        => property.FindAnnotation(RelationalAnnotationNames.JsonPropertyName)?.GetConfigurationSource();
 }

src/EFCore.Relational/Metadata/Conventions/RelationalNavigationJsonPropertyNameAttributeConvention.cs
@@ -1,0 +1,51 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text.Json.Serialization;
+
+namespace Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+/// <summary>
+///     A convention that configures a JSON element name for the navigation property mapped to json
+///     based on the <see cref="JsonPropertyNameAttribute" /> attribute.
+/// </summary>
+/// <remarks>
+///     See <see href="https://aka.ms/efcore-docs-conventions">Model building conventions</see> for more information and examples.
+/// </remarks>
+public class RelationalNavigationJsonPropertyNameAttributeConvention : NavigationAttributeConventionBase<JsonPropertyNameAttribute>
+{
+    /// <summary>
+    ///     Creates a new instance of <see cref="RelationalNavigationJsonPropertyNameAttributeConvention" />.
+    /// </summary>
+    /// <param name="dependencies">Parameter object containing dependencies for this convention.</param>
+    /// <param name="relationalDependencies"> Parameter object containing relational dependencies for this convention.</param>
+    public RelationalNavigationJsonPropertyNameAttributeConvention(
+        ProviderConventionSetBuilderDependencies dependencies,
+        RelationalConventionSetBuilderDependencies relationalDependencies)
+        : base(dependencies)
+    {
+        RelationalDependencies = relationalDependencies;
+    }
+
+    /// <summary>
+    ///     Relational provider-specific dependencies for this service.
+    /// </summary>
+    protected virtual RelationalConventionSetBuilderDependencies RelationalDependencies { get; }
+
+    /// <summary>
+    ///     Called after a navigation property that has an attribute is added to an entity type.
+    /// </summary>
+    /// <param name="navigationBuilder">The builder for the navigation.</param>
+    /// <param name="attribute">The attribute.</param>
+    /// <param name="context">Additional information associated with convention execution.</param>
+    public override void ProcessNavigationAdded(
+        IConventionNavigationBuilder navigationBuilder,
+        JsonPropertyNameAttribute attribute,
+        IConventionContext<IConventionNavigationBuilder> context)
+    {
+        if (!string.IsNullOrWhiteSpace(attribute.Name))
+        {
+            navigationBuilder.HasJsonPropertyName(attribute.Name, fromDataAnnotation: true);
+        }
+    }
+}